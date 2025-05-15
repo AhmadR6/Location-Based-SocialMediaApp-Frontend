2ec6025ebdd2989d0f5c6166bae36c1204fdf904
Ahmad Rehman <ahmadrehman851@gmail.com>
2025-05-16 02:19:18 +0500

    Refactor TextBubble and GroupChat components: enhance message display with sender username, improve socket connection handling, and optimize message fetching and rendering logic.

--- src/components/DM/TextBubble.jsx
+++ src/components/DM/TextBubble.jsx
@@ -3,11 +3,15 @@ import "./textBubble.scss";
 
 const TextBubble = ({ message }) => {
   const formattedDate = format(message.createdAt, "h:mm aaa");
+  const senderUsername = message.zoneId ? message.sender.username : null;
 
   return (
-    <div className={message.fromUser ? "bubble right" : "bubble left"}>
-      <p>{message.content}</p>
-      <p className="time">{formattedDate}</p>
+    <div className="bubble-wraper">
+      {!message.fromUser && <p className="username">{senderUsername}</p>}
+      <div className={message.fromUser ? "bubble right" : "bubble left"}>
+        <p>{message.content}</p>
+        <p className="time">{formattedDate}</p>
+      </div>
     </div>
   );
 };
