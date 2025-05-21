54bb9f555c89405676c796c0168b3e254279b11b
Ahmad Rehman <ahmadrehman851@gmail.com>
2025-05-21 18:25:53 +0500

    Add CreatePage component for post and event creation with navigation buttons

---
+++ src/pages/create/create.jsx
@@ -0,0 +1,27 @@
+import { useNavigate } from "react-router-dom";
+import "./CreatePage.scss";
+
+const CreatePage = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="content">
+      <div className="main-content">
+        <button
+          className="create-btn"
+          onClick={() => navigate("/p/create/post")}
+        >
+          Create New Post
+        </button>
+        <button
+          className="create-btn"
+          onClick={() => navigate("/p/create/events")}
+        >
+          Create New Event
+        </button>
+      </div>
+      <div className="side-content"></div>
+    </div>
+  );
+};
+
+export default CreatePage;
